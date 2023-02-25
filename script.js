8468c1cdc05460373ac6a839526d2f2a299fcc61
Adnanarodiya <aiarodiya07@gmail.com>
2023-02-25 09:21:30 +0530

    completed

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const newGame = document.querySelector('.btn--new');
 
 //starting condition
 score0El.textContent = 0;
@@ -24,10 +25,27 @@ const switchPlayer = () => {
   player1El.classList.toggle('player--active');
 };
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let scores, currentScore, activePlayer, playing;
+
+//New game starting condition
+const init = () => {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player0El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', () => {
@@ -77,3 +95,5 @@ btnHold.addEventListener('click', () => {
     switchPlayer();
   }
 });
+
+btnNew.addEventListener('click', init);
